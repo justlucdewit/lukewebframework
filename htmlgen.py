6bc361e1db79e3c315a502348b4091e23d28a57a
Luc de wit <luc.dewit@hotmail.nl>
2020-01-07 10:04:09 +0100

    parser v3

--- htmlgen.py
+++ htmlgen.py
@@ -6,12 +6,25 @@ errorCodes = [
     "[ERROR] found an element of type {} closed with an {} tag on line {}"  #1
 ]
 
+class Component:
+    def __init__(self):
+        pass
+
+    def render():
+        pass
+
 class HTML:
     def __init__(self, type="dom"):
         #set elemental properties
         self.type = type
+        self.istext = False
+        self.textContent = ""
+        
         self.children = []
         self.haschildren = False
+        
+        self.arguments = []
+        
 
 def HTMLparse(string):
     DOM = HTML()
@@ -22,8 +35,17 @@ def HTMLparse(string):
         string = string.splitlines()
         for lineNumber, tag in enumerate(string):
             content = tag.strip()
-            if content[0] == "<" and content[1] != "!":#if content is a tag and isnt doctype
-                tagname = content[1:-1]
+            if content[1] != "!":#if content is a tag and isnt doctype
+                if content[0] == "<":
+                    tagname = content[1:-1]
+                else:
+                    tagname = content
+                    head.haschildren = True
+                    txt = HTML("text")
+                    txt.istext = True
+                    txt.textContent = tagname
+                    head.children.append(txt)
+                    continue
                 if tagname[0] != "/":#if its not a closing tag
                     head.haschildren = True
                     head.children.append(HTML(tagname))
@@ -31,6 +53,7 @@ def HTMLparse(string):
                     head = head.children[-1]
                 else:#a closing tag has been found
                     if tagname[1:] != head.type:
+                        print(head.istext)
                         errors.append(errorCodes[1].format(head.type, tagname[1:], lineNumber))
                         return DOM, errors
                     head = DOMHIST.pop()
@@ -41,8 +64,11 @@ def printHTMLTree(DOM, indent=0):
         for element in DOM.children:
             indentation = ""
             for i in range(indent):
-                indentation+="  "
-            print(f"{indentation}{element.type}")
+                indentation+="| "
+            if not element.type == "text":
+                print(f"{indentation}<{element.type}>")
+            else:
+                print(f"{indentation}{element.textContent}")
             if element.haschildren:
                 printHTMLTree(element, indent+1)
 
@@ -51,3 +77,6 @@ def renderString(string, output):
         File.write(string)
 
     webbrowser.open(f'file://{os.path.realpath(output)}')
+
+def renderDOM(DOM, outputfile):
+    pass
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ website = '''<!DOCTYPE html>
     </head>
     <body>
         this is a website generated with my own framework
-    </nav>
+    </body>
 </html>
 '''
 
@@ -20,19 +20,4 @@ for err in errors:
 
 print()
 printHTMLTree(DOM)
-
 #renderString(website, "test.html")
-
-
-class Women:
-    def __init__(self, age):
-        self.inside = []
-
-    def cook(self):
-        pass
-    
-    def insertObject(self, obj):
-        self.inside.append(obj)
-
-    def pleasureOther(self, other):
-        self.insertObject(self, other.dick())
